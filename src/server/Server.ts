5dd2e5525b2f3d5875bcc536a68f94f51b5e8d36
Kostyantyn Proskuryakov <2399977+KProskuryakov@users.noreply.github.com>
2020-10-16 17:22:44 -0400

    Working livereload

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,13 +1,24 @@
 import express from 'express';
 import path from 'path';
 import http from 'http';
-// import helmet from 'helmet';
+import helmet from 'helmet';
 import WebSocket from 'ws';
 
 const port = 8080;
 
 const app = express();
-// app.use(helmet());
+
+// disable CSP if not in production for livereload to work
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    helmet({
+      contentSecurityPolicy: false
+    })
+  );
+} else {
+  app.use(helmet());
+}
+
 app.use(express.static(path.join(__dirname, '../../public')));
 console.log(__dirname);
 
@@ -15,7 +26,7 @@ const server = http.createServer(app);
 
 const wss = new WebSocket.Server({ server });
 
-const activeSockets = new Set<WebSocket>()
+const activeSockets = new Set<WebSocket>();
 
 wss.on('connection', function connection(ws) {
   activeSockets.add(ws);
