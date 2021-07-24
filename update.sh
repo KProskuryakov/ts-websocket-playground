88193b78b8089ca564fa573a29a35d54a61df7fe
kproskuryakov <2399977+KProskuryakov@users.noreply.github.com>
2021-07-24 15:55:49 -0400

    Update update script

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 git pull
 sudo cp saunterdev.service /etc/systemd/system/saunterdev.service
+sudo chown root:root /etc/systemd/system/saunterdev.service
+sudo chmod 644 /etc/systemd/system/saunterdev.service
 sudo systemctl daemon-reload
 sudo systemctl restart saunterdev
 sudo systemctl enable saunterdev
